67bce641c59565444413af6e256c50e45df7b620
Ryan-Nam <alfofh7060@gmail.com>
2023-07-31 23:07:22 +1000

    Add components

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
-import Image from 'next/image';
+import Hero from '@/components/home/hero';
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Home</h1>
+    <section className="flex flex-col min-h-screen items-center justify-center text-gray-600 body-font">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <Hero />
+        </div>
+      </section>
+      
     </>
   );
 }
